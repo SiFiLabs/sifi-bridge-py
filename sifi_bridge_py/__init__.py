baf7f0f5cc0ed5e8073c7d68f3da1e4f9bafedaf
Gabriel Gagne <gabrielpgagne@gmail.com>
2024-09-22 15:45:28 -0400

    1.1.2 and re-export EmgNotch enum, update readme

--- sifi_bridge_py/__init__.py
+++ sifi_bridge_py/__init__.py
@@ -1,9 +1,11 @@
+# Must do it this way to avoid re-exporting sifi_bridge's imports
 from .sifi_bridge import (
-    SifiBridge,  # noqa
-    ListSources,  # noqa
-    DeviceType,  # noqa
     DeviceCommand,  # noqa
+    DeviceType,  # noqa
     BleTxPower,  # noqa
     MemoryMode,  # noqa
+    EmgNotch,  # noqa
     PpgSensitivity,  # noqa
+    ListSources,  # noqa
+    SifiBridge,  # noqa
 )
